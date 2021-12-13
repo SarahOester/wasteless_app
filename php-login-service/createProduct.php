fd87c7c54285c84f5071f18786942b4084afe056
Idakronborgn <55064627+Idakronborgn@users.noreply.github.com>
2021-12-13 09:46:36 +0100

    create til database

--- php-login-service/createProduct.php
+++ php-login-service/createProduct.php
@@ -1,7 +1,4 @@
 <?php
-    var_dump($_POST);
-    // ['title']
-    exit;
 
     // Start sessions
     session_start();
@@ -12,47 +9,19 @@
     // |         Get form data to php and database          |
     // +----------------------------------------------------+
 
-  /*  if (isset($_GET['action'])) {
-    $action = $_GET['action'];
 
-        // CREATE PRODUTCS
-        if ($action == "signup") {
-            $loginObject = json_decode(file_get_contents('php://input'));
-            $avatarImg = $loginObject->avatarImg;
-            $title = $loginObject->title;
-            $businessName = $loginObject->businessName;
-            $category = $loginObject->category;
-            $productDes = $loginObject->productDes;
-            $price = $loginObject->price;
-            $pickupDate = $loginObject->pickupDate;
-            $pickupTime = $loginObject->pickupTime;
-
-            //Put into database
-            $sql = "CALL CreateProduct('$avatarImg', '$title', '$businessName', '$category', '$productDes', '$price', '$pickupDate', '$pickupTime')";
-                        if ($mySQL->query($sql) === TRUE) {
-                            $response['newProductCreated'] = TRUE;
-                            echo json_encode($response);
-                        } else {
-                            $response['newProductCreated'] = FALSE;
-                            $response['error'] = "Was not able to create a new product.";
-                            echo json_encode($response);
-                        }
-            }
-    }
-
-*/
-
-    $con = new mysqli($server,$username,$password,$database);
+    $con = new mysqli($server, $username, $password, $database);
     if(!$con) {
         echo "Håber det her virker";
         // tjekker om serveren er connecet for at se om der kommer nogle errors
     }
-    
+
     $businessName = $_POST['businessName'];
     $title = $_POST['title'];
     //samler form dataen i variables
     //Her indsætter vi det til vores table
     $qry = "INSERT INTO `products`(`businessName`,`title`) VALUES ('$businessName','$title')";
+
     
     $insert = mysqli_query($con,$qry);
     if(!$insert) {
@@ -75,9 +44,6 @@
     // $sql = "Call CreateProduct('avatarimgVar', 'Brød2', 'Brødhus2', 'Brød kate', 'kdsjfsjdhks', 12, '2021-12-01', '12:12:12')";
     // $mySQL->query($sql);
     
-
-
-
     // // Get access to the FileUpload Class
     // require "src/fileUpload.php";
 
